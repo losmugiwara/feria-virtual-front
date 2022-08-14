4ab078c1d539b3bd6dbcc3d88513ff2978de3545
Hugo Fica <hugo.fica.saez@gmail.com>
2022-08-13 20:52:48 -0400

    bases de feria virtual

---
+++ src/FeriaVirtual.jsx
@@ -0,0 +1,8 @@
+
+export const FeriaVirtual = () => {
+  return (
+    <>
+        <h1>Feria Virtual</h1>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FeriaVirtual } from './FeriaVirtual';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FeriaVirtual/>
+  </React.StrictMode>
+)
