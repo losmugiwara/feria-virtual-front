bdd8c29cb83603ba877679fdf68dd673005f64f8
Hugo Fica <hugo.fica.saez@gmail.com>
2022-08-28 01:27:59 -0400

    arreglos

---
+++ src/feriaVirtual/components/ui/NavBar.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from 'react-router-dom'
+
+import './ui.css';
+import logo from '../../../img/logoMaipoGrande.png'
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container-fluid d-flex">
+                <NavLink className="navbar-brand" to='/'>
+                    <img className='logo' src={logo} />
+                </NavLink>
+                <div className="d-flex justify-content-end font-Inter" id="navbarNav">
+                    <ul className="navbar-nav d-flex flex-row">
+                        <li className="nav-item p-1">
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active text-active fw-semibold' : ''}`}
+                                to='/'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item p-1">
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active text-active fw-semibold' : ''}`}
+                                to='productos'>
+                                Productos
+                            </NavLink>
+                        </li>
+                        <li className="nav-item p-1">
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active text-active fw-semibold' : ''}`}
+                                to='transporte'>
+                                Transporte
+                            </NavLink>
+                        </li>
+                        <li className="nav-item p-1">
+                            <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active text-active fw-semibold' : ''}`}
+                                to='login'>
+                                Cuenta
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
