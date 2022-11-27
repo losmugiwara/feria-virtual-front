1b4e973070659d02d19900ddf3f065ac18794f72
furibe_vt <furibe@vtsystems.cl>
2022-11-27 16:51:30 -0300

    tabla con transportistas

--- src/auth/components/adminMG/proceso-ventas/subastas/AuctionDetail.jsx
+++ src/auth/components/adminMG/proceso-ventas/subastas/AuctionDetail.jsx
@@ -1,6 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getAuctionById } from '../../../../../helpers/auctions';
+//importaciones
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
 
 export const AuctionDetail = () => {
 
@@ -18,8 +27,25 @@ export const AuctionDetail = () => {
             setAuction();
         }
     }, []);
-    
 
+
+    function createData(name, lastName, offer) {
+        return { name, lastName, offer };
+    }
+
+    let rows = [
+        // createData('Francisco', 'Uribe', 159),
+        // createData('Francisco', 'Uribe', 159),
+        // createData('Francisco', 'Uribe', 159),
+        // createData('Francisco', 'Uribe', 159),
+        // createData('Francisco', 'Uribe', 159),
+        // createData('Francisco', 'Uribe', 159)
+
+    ];
+
+    auction?.carrierOffers.map((c) => {
+        rows.push(createData(c.user.name, c.user.lastName, c.offer))
+    });
 
 
     return (
@@ -27,7 +53,45 @@ export const AuctionDetail = () => {
             <h3>Subasta</h3>
             <hr />
 
-          
+
+            <Typography align='center' variant='h5' sx={{mb: 3}}>
+                Ofertas realizadas por transportistas
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><b>#</b></TableCell>
+                            <TableCell><b>Nombre</b></TableCell>
+                            <TableCell><b>Apellido</b></TableCell>
+                            <TableCell><b>Oferta</b></TableCell>
+
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, index) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {index + 1}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {row.lastName}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {row.offer}
+                                </TableCell>
+
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
 
         </div>
     )
